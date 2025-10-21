f199f8c7e94d9f7d5d6b9336d5cc2b1dca2adeff
Martin Georgiev <3349414+Thinkorswim@users.noreply.github.com>
2025-10-21 17:19:10 +0300

    Bump SimFace version number

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 val projectGroupId = "com.simprints.biometrics"
 val projectArtifactId = "simface"
-val projectVersion = "2025.3.1"
+val projectVersion = "2025.3.2"
 
 group = projectGroupId
 version = projectVersion
