e56091fde85eef2c9031ecfa5c6aa2a963966ec2
Martin Georgiev <3349414+Thinkorswim@users.noreply.github.com>
2025-10-28 11:51:02 +0200

    Add SimQ publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 val projectGroupId = "com.simprints.biometrics"
 val projectArtifactId = "simface"
-val projectVersion = "2025.3.2"
+val projectVersion = "2025.3.3"
 
 group = projectGroupId
 version = projectVersion
